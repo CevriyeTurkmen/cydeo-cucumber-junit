cbed17951bfd881f052fd7cc89cc5f725ae4f18b
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2023-02-25 19:53:24 +0000

    Changes

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -4,8 +4,11 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -45,10 +48,31 @@ public class BrowserUtils {
         Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
     }
 
+    public static void verifyURLContains(String expectedinURL){
+
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedinURL));
+
+    }
+
     public static void waitForInvisibilityOf(WebElement webElement){
         Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
         wait.until(ExpectedConditions.invisibilityOf(webElement));
     }
+
+    public static List<String>dropdownOptionsAsString(WebElement dropdownElement){
+
+        Select select=new Select(dropdownElement);
+        List<WebElement>actualOptionsAsElement=select.getOptions();
+        List<String>actualOptionsAsString=new ArrayList<>();
+
+        for (WebElement each : actualOptionsAsElement) {
+            actualOptionsAsString.add(each.getText());
+        }
+
+        return actualOptionsAsString;
+    }
+
+
 
 }
--- src/test/resources/features/webTableLogin.feature
+++ src/test/resources/features/webTableLogin.feature
@@ -1,9 +1,23 @@
+
 Feature: User should be able to login using correct credentials
 
-  @wip
-  Scenario: Positive login scenariq
-    Given user is on the login page of web table app
+Background: User is on the login page
+  Given user is on the login page of web table app
+
+  Scenario: Positive login scenario
+
     When user enters username "Test"
     And user enters password "Tester"
-    And user clicks the login button
-    Then user should see URL contains orders
+    And user clicks to login button
+    Then user should see url contains orders
+
+  Scenario: Positive login scenario#2
+    When user enters username "Test" password "Tester" and logins
+    Then user should see url contains orders
+
+
+  Scenario: User should be able to see all 12 months in months dropdown
+    When User enters below credentials
+      | username     | Test   |
+      | password     | Tester |
+    Then user should see url contains orders
